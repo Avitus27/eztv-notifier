88c9e09fb7bdac42723981ee1d80f15bf5e6cb42
Avitus27 <mervyn511@gmail.com>
2017-05-15 17:31:17 +0100

    reorder code (PEP8), add comments hinting at what's to be done

--- Main.py
+++ Main.py
@@ -9,20 +9,18 @@ from email.mime.text import MIMEText
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-# Create a text/plain message
-msg = MIMEText("This is a test message")
-
 recipient = os.environ.get("RECIPIENT")
 from_email = os.environ.get("FROM_EMAIL")
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'This is a test message'
+
+# get JSON from EZTV
+
+# process it, checking for new episodes of the chosen shows
+
+msg = MIMEText("This is a test message") # To be replaced with details of the new show and links
+msg['Subject'] = 'New episodes have been found!'
 msg['From'] = from_email
 msg['To'] = recipient
 
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
 s = smtplib.SMTP('localhost')
 s.sendmail(from_email, recipient, msg.as_string())
 s.quit()
-
