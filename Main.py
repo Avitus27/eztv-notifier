3b954c12653e071d4169cd0cad015e38745bfb9d
Mervyn Galvin <mervyn511@gmail.com>
2017-11-20 22:21:31 +0000

    Ready for release v1.0
    remove authemail
    remove un-needed imports
    check python version, currently only works for 2.7
    add option for 'title: magnet' format as gmail strips magnet links from the full rich-text
    remove debugging code
    give (meaningful) exit codes
    emails now work, refactor email code to conform to DRY

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 
-import smtplib
 import os
-import authemail
 import json
 import requests
 import sys
 
 from os.path import join, dirname
-from dotenv import load_dotenv
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+from smtplib import SMTP
+
+if sys.version_info[0] > 2:
+    print("Python3 not fully supported yet")
+    exit(1)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -19,6 +23,10 @@ from_email = os.environ.get("FROM_EMAIL")
 mail_host = os.environ.get("MAIL_HOST")
 mail_port = os.environ.get("MAIL_PORT")
 mail_subject = os.environ.get("MAIL_SUBJECT")
+if not os.environ.get("FULL_RICH_MAIL"):
+    rich_mail = False
+else:
+    rich_mail = True
 if not os.environ.get("USE_SMTP"):
     use_smtp = False
     username = os.environ.get("MAIL_USER")
@@ -29,12 +37,10 @@ else:
 max_torrents = int(os.environ.get("MAX_TORRENTS"))
 
 show_list = os.environ.get("SHOW_LIST")
-
 show_list = show_list.split(",")
 
 file = open('last_torrent', 'r')
 last_seen_torrent = int(file.readline())
-#print( last_torrent )
 file.close()
 
 # get JSON from EZTV
@@ -52,39 +58,33 @@ if request[-1].status_code == 200:
     file.close()
 else:
     print(request[-1].status_code)
-    exit()
+    exit(1)
 
-#last_fetched_torrent_id = sys.maxint
-# Need to think about using this or the above
 last_fetched_torrent_id = [0]
 last_fetched_torrent_id[0] = int(
     request[-1].json()['torrents'][max_torrents - 1]['date_released_unix'])
 torrent_found = False
 page = 1
-message_text = ""
-
-print(
-    "last_seen_torrent: " +
-    str(last_seen_torrent) +
-    "\r\nlast_fetched_torrent_id: " +
-    str(last_fetched_torrent_id[0])
-	)
+plain_text = "New Torrents available:\n"
+rich_text = "New Torrents available:<br>\n"
 
 while last_fetched_torrent_id[0] > last_seen_torrent:
-    # TODO: These lines need to be redone.
     print("Page: " + str(page))
     print("last_seen_torrent: " + str(last_seen_torrent))
     print("last_fetched_torrent_id: " + str(last_fetched_torrent_id[0]))
     for torrent in request[-1].json()['torrents']:
         if any(show in torrent['title'] for show in show_list):
             torrent_found = True
-            # print(torrent)
-            print("Torrent Found!")
-            message_text += "<a href=\"" + \
-                str(torrent['magnet_url']) + "\">" + \
-                str(torrent['title']) + "</a>\r\n"
+            if rich_mail:
+                rich_text += "<a rel=\"nofollow\" href=\"" + \
+                    str(torrent['magnet_url']) + "\">" + \
+                    str(torrent['title']) + "</a><br>\r\n"
+            else:
+                rich_text += str(torrent['title']) + \
+                    ":\t" + str(torrent['magnet_url']) + "<br><br>\r\n"
+            plain_text += str(torrent['title']) + "\t" + \
+                str(torrent['magnet_url']) + "\r\n\r\n"
 
-#        if request.json()['torrents'][id]['title']
     last_fetched_torrent_id[0] = int(request[-1].json(
     )['torrents'][max_torrents - 1]['date_released_unix'])
     page += 1
@@ -94,29 +94,24 @@ while last_fetched_torrent_id[0] > last_seen_torrent:
             str(max_torrents) +
             '&page=' +
             str(page)))
-# process it, checking for new episodes of the chosen shows
 
 if not torrent_found:
     print("None found")
-    exit()
+    exit(0)
 
-# To be replaced with details of the new show and links
-msg = MIMEText(message_text)
+msg = MIMEMultipart('alternative')
 msg['Subject'] = mail_subject
 msg['From'] = from_email
 msg['To'] = recipient
+part1 = MIMEText(plain_text, 'plain')
+part2 = MIMEText(rich_text, 'html')
+msg.attach(part1)
+msg.attach(part2)
 
-if use_smtp:
-    s = smtplib.SMTP(mail_host)
-    s.sendmail(from_email, recipient, msg.as_string())
-    s.quit()
-else:
-    authemail.sendemail(
-        mail_subject,
-        recipient,
-        from_email,
-        mail_host,
-        mail_port,
-        username,
-        password,
-        message_text)
+s = SMTP(mail_host)
+
+if not use_smtp:
+    s.login(username, password)
+
+s.sendmail(from_email, recipient, msg.as_string())
+s.quit()
