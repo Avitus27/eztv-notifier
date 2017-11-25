52b74ec75b64292a6de3c5ece2c874458ac6a98e
Mervyn Galvin <mervyn511@gmail.com>
2017-11-25 01:37:06 +0000

    Add argument parser and arguments, start implimenting them

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import os
 import json
 import requests
 import sys
+import argparse
 
 from os.path import join, dirname
 from email.mime.text import MIMEText
@@ -11,11 +12,51 @@ from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 from smtplib import SMTP
 
+##RECIPIENT="admin@localhost"
+##FROM_EMAIL="admin@localhost"
+##USE_SMTP="True" # Use an empty string for False, anything else for True
+##MAIL_HOST="localhost"
+##MAIL_PORT=25
+##MAIL_USER="user"
+##MAIL_PASS="password"
+##MAIL_SUBJECT="New episodes have been found!"
+##FULL_RICH_MAIL=""
+##
+##MAX_TORRENTS=10 # Max number of torrents per request to EZTV
+### lower value results in smaller, but possibly more, responses
+##
+##SHOW_LIST="XYZ,ABC"
+
+parser = argparse.ArgumentParser(description="Python based tool for getting torrents of shows from eztv", prog="eztv-notifier")
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('-v', '--verbose', action="store_true", help="Generates more output for debugging")
+parser.add_argument('-e', '--env', help="Sets the path to the .env file. Default is the directory the file is running in.")
+parser.add_argument('-r', '--recipient', help="Specifies what email address the script should send to.")
+parser.add_argument('-f', '--from', help="Specifies what email address the script should send from.")
+parser.add_argument('--smtp', action="store_true", help="If set, the script will try using SMTP instead of logging in.")
+parser.add_argument('--host', help="Specifies the address of the mail host to send from. IP:PORT format not accepted")
+parser.add_argument('--port', help="Specifies the port of the mail host to send from.")
+parser.add_argument('-u', '--user', help="The username to log into the mail host. Not used for SMTP.")
+parser.add_argument('-p', '--password', help="The password to log into the mail host. Not used for SMTP.")
+parser.add_argument('-s', '--subject', help="The subject line of the email to be sent.")
+parser.add_argument('--rich', action="store_true", help="If set, the email will be formatted with full HTML. May not work in all clients (GMail strips magnet links for example).")
+parser.add_argument('--max', help="The max number of torrents to get per requeset to eztv.")
+parser.add_argument('--shows', help="A list of the shows to search for on eztv.")
+
+
+verbose = False;
+args = parser.parse_args()
+if args.verbose:
+	verbose = True
+
 if sys.version_info[0] > 2:
     print("Python3 not fully supported yet")
     exit(1)
 
-dotenv_path = join(dirname(__file__), '.env')
+if args.env:
+	dotenv_path = join(dirname(args.env), '.env')
+else:
+	dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 recipient = os.environ.get("RECIPIENT")
@@ -93,7 +134,6 @@ while last_fetched_torrent_id[0] > last_seen_torrent:
             str(page)))
 
 if not torrent_found:
-    print("None found")
     exit(0)
 
 msg = MIMEMultipart('alternative')
