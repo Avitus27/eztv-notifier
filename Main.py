86592475edca6efa54af711fee24a82bb6dd80be
Mervyn Galvin <mervyn511@gmail.com>
2017-11-25 02:03:35 +0000

    remove comments, update parts of parser helptext, add some processing of arguments

--- Main.py
+++ Main.py
@@ -12,68 +12,67 @@ from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 from smtplib import SMTP
 
-##RECIPIENT="admin@localhost"
-##FROM_EMAIL="admin@localhost"
-##USE_SMTP="True" # Use an empty string for False, anything else for True
-##MAIL_HOST="localhost"
-##MAIL_PORT=25
-##MAIL_USER="user"
-##MAIL_PASS="password"
-##MAIL_SUBJECT="New episodes have been found!"
-##FULL_RICH_MAIL=""
-##
-##MAX_TORRENTS=10 # Max number of torrents per request to EZTV
-### lower value results in smaller, but possibly more, responses
-##
-##SHOW_LIST="XYZ,ABC"
+if sys.version_info[0] > 2:
+    print("Python3 not fully supported yet")
+    exit(1)
 
-parser = argparse.ArgumentParser(description="Python based tool for getting torrents of shows from eztv", prog="eztv-notifier")
+parser = argparse.ArgumentParser(description="Python based tool for getting torrents of shows from eztv", prog="eztv-notifier", epilog="Values passed as arguments override the .env")
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 parser.add_argument('-v', '--verbose', action="store_true", help="Generates more output for debugging")
-parser.add_argument('-e', '--env', help="Sets the path to the .env file. Default is the directory the file is running in.")
+parser.add_argument('-e', '--env', nargs='?', help="Sets the path to the .env file. If the string is 'False', then the program will not look for a .env file (not recommended). Default is the directory the file is running in.")
 parser.add_argument('-r', '--recipient', help="Specifies what email address the script should send to.")
-parser.add_argument('-f', '--from', help="Specifies what email address the script should send from.")
+parser.add_argument('-s', '--sender', help="Specifies what email address the script should send from.")
 parser.add_argument('--smtp', action="store_true", help="If set, the script will try using SMTP instead of logging in.")
 parser.add_argument('--host', help="Specifies the address of the mail host to send from. IP:PORT format not accepted")
 parser.add_argument('--port', help="Specifies the port of the mail host to send from.")
 parser.add_argument('-u', '--user', help="The username to log into the mail host. Not used for SMTP.")
 parser.add_argument('-p', '--password', help="The password to log into the mail host. Not used for SMTP.")
-parser.add_argument('-s', '--subject', help="The subject line of the email to be sent.")
+parser.add_argument('--subject', help="The subject line of the email to be sent.")
 parser.add_argument('--rich', action="store_true", help="If set, the email will be formatted with full HTML. May not work in all clients (GMail strips magnet links for example).")
 parser.add_argument('--max', help="The max number of torrents to get per requeset to eztv.")
 parser.add_argument('--shows', help="A list of the shows to search for on eztv.")
 
 
-verbose = False;
+verbose = False
+setup_error = False
+use_env = True
 args = parser.parse_args()
 if args.verbose:
 	verbose = True
 
-if sys.version_info[0] > 2:
-    print("Python3 not fully supported yet")
-    exit(1)
-
-if args.env:
-	dotenv_path = join(dirname(args.env), '.env')
+if args.env == "False":
+	use_env = False
+if use_env:
+	if args.env:
+		dotenv_path = join(dirname(args.env), '.env')
+	else:
+		dotenv_path = join(dirname(__file__), '.env')
+	load_dotenv(dotenv_path)
+	recipient = os.environ.get("RECIPIENT")
+	from_email = os.environ.get("FROM_EMAIL")
+	mail_host = os.environ.get("MAIL_HOST")
+	mail_port = os.environ.get("MAIL_PORT")
+	mail_subject = os.environ.get("MAIL_SUBJECT")
+	rich_mail = os.environ.get("FULL_RICH_MAIL") or args.rich
+	use_smtp = os.environ.get("USE_SMTP") or args.smtp
 else:
-	dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+	rich_mail = args.rich
+	use_smtp = args.smtp
 
-recipient = os.environ.get("RECIPIENT")
-from_email = os.environ.get("FROM_EMAIL")
-mail_host = os.environ.get("MAIL_HOST")
-mail_port = os.environ.get("MAIL_PORT")
-mail_subject = os.environ.get("MAIL_SUBJECT")
-if not os.environ.get("FULL_RICH_MAIL"):
-    rich_mail = False
-else:
-    rich_mail = True
-if not os.environ.get("USE_SMTP"):
-    use_smtp = False
+
+if args.recipient:
+	recipient = args.recipient
+if args.sender:
+	from_email = args.sender
+if args.host:
+	mail_host = args.host
+if args.port:
+	mail_port = args.port
+if args.subject:
+	mail_subject = args.subject
+if not use_smtp:
     username = os.environ.get("MAIL_USER")
     password = os.environ.get("MAIL_PASS")
-else:
-    use_smtp = True
 
 max_torrents = int(os.environ.get("MAX_TORRENTS"))
 
